26e1242acca82b7753bf0b7df2b4af2116d50514
Iker14 <145350571+Iker14@users.noreply.github.com>
2024-05-26 00:09:21 +0200

    s

--- ruleta.js
+++
@@ -1,7 +0,0 @@
-// ruleta.js
-$(document).ready(function() {
-    $('#wheel').click(function() {
-      var deg = Math.floor(360 * Math.random());
-      $(this).css('transform', 'rotate(' + deg + 'deg)');
-    });
-  });
